b3e6bb2380728edb3bec507de53ecd71d0f0e24b
kimthongtomosia <thong.le@tomosia.com>
2025-04-15 14:07:23 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+const model = defineModel()
+
+function update() {
+  model.value++
+}
+</script>
+
+<template>
+  <div>Parent bound v-model is: {{ model }}</div>
+  <button @click="update">Increment</button>
+</template>
+
+<style scoped></style>
